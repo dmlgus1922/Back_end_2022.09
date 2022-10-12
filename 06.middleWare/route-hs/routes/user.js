fb5f51c9576b71d7a6d0df887601c4b34f728984
dmlgus1922 <dmlgus1922@naver.com>
2022-10-12 16:21:16 +0900

    221012

--- 06.middleWare/route-hs/routes/user.js
+++ 06.middleWare/route-hs/routes/user.js
@@ -2,9 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
-// GET /user 라우터
-router.get('/', (req, res) => {
-  res.send('Hello, User');
-});
+// // GET /user 라우터
+// router.get('/:id', (req, res) => {
+//   res.send('GET /:id is ' + req.params);
+//   console.log(req.params, req.query);
+// })
+// .post('/:id', (req, res) => {
+//   res.send('POST /abc');
+//   console.log(req.params, req.query);
+// });
+
+router.route('/:id')
+.get(
+  (req, res) => {
+    res.send('GET /:id is ' + req.params['id']);
+  }
+)
+.post(
+  (req, res) => {
+    res.send('POST /:id');
+  }
+)
+;
+
 
 module.exports = router;
