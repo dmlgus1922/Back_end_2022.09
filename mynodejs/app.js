8a7181d8f4a30bfe10da10dd5e7718daf719bf56
dmlgus1922 <dmlgus1922@naver.com>
2022-10-17 17:58:26 +0900

    221017

---
+++ mynodejs/app.js
@@ -0,0 +1,13 @@
+const express = require('express');     // 설치된 express 사용 선언
+const app = express();   // express를 실행해 app 변수에 대입
+                         // 127.0.0.1  포트번호가 없음
+const router = require('./router/router.js');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended : false}));
+// post방식일 때 body영역을 분석해주는 미들웨어로 bodyparser 등록
+app.use(router);    // 미들웨어로 router를 등록
+
+app.listen(3000);   // 현재 서버 파일의 port번호 등록
+
---
+++ mynodejs/router/router.js
@@ -0,0 +1,91 @@
+const express = require('express');
+
+const router = express.Router();    // express가 갖고 있는 기능 중 router를 사용
+
+router.get('/plus', function(request, response) {   // plus 라우터 기능 정의 및 등록
+    console.log('/plus 라우터 호출');
+    console.log(parseInt(request.query.num1) + parseInt(request.query.num2));
+    
+    // 응답할 파일 지정. html 만들기
+    response.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+    // 그 파일에 내용 넣기
+    response.write('<html>');
+    response.write('<body>');
+    response.write('응답 성공<br>');
+    response.write('결과값 : ' + (parseInt(request.query.num1) + parseInt(request.query.num2)));
+    response.write('</body>');
+    response.write('</html>');
+    // 사용자에게 보여주기
+    response.end();
+});
+
+router.get('/cal', function(request, response) {    // cal 라우터 기능 정의 및 등록
+    // 1. 사용자가 입력한 값을 가져오기.
+    let num1 = parseInt(request.query.num1);
+    let num2 = parseInt(request.query.num2);
+    let cal = request.query.cal;
+    let result
+    
+    if (cal == '+'){
+        result = num1 + num2;
+    } else if (cal == '-') {
+        result = num1 - num2;
+    } else if (cal == '*') {
+        result = num1 * num2;
+    } else {
+        result = num1 / num2;
+    }
+
+    // 사용자가 입력한 기호에 맞는 연산 결과값을 브라우저에 출력
+    response.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+    // 그 파일에 내용 넣기
+    response.write('<html>');
+    response.write('<body>');
+    response.write('결과값 : ' + result);
+    response.write('</body>');
+    response.write('</html>');
+    // 사용자에게 보여주기
+    response.end();
+});
+
+router.post('/Grade', (request, response)=> {
+    const name = request.body.name;
+    const html = parseInt(request.body.html);
+    const css = parseInt(request.body.css);
+    const nodejs= parseInt(request.body.nodejs);
+    const android = parseInt(request.body.android);
+
+    const avg = (html + css + nodejs + android) / 4;
+
+    let grade;
+    if (avg >= 95) {
+        grade = 'A+';
+    } else if (avg >= 90) {
+        grade = 'A';
+    } else if (avg >= 85) {
+        grade = 'B+';
+    } else if (avg >= 80) {
+        grade = 'B';
+    } else if (avg >= 75) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    response.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+    response.write('<html>');
+    response.write('<body>');
+    response.write('name : ' + name + '<br>');
+    response.write('html : ' + html + '<br>');
+    response.write('css : ' + css + '<br>');
+    response.write('nodejs : ' + nodejs + '<br>');
+    response.write('android : ' + android + '<br>');
+    response.write('avg : ' + avg + '<br>');
+    response.write('grade : ' + grade);
+    response.write('</body>');
+    response.write('</html>');
+    response.end();
+
+});
+
+module.exports = router;
