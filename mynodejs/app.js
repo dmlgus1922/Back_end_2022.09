4c2fd6c3a820a945ae33ceb522625a16145377bc
dmlgus1922 <dmlgus1922@naver.com>
2022-10-20 17:38:22 +0900

    221020

--- mynodejs/app.js
+++ mynodejs/app.js
@@ -2,12 +2,20 @@ const express = require('express');     // 설치된 express 사용 선언
 const app = express();   // express를 실행해 app 변수에 대입
                          // 127.0.0.1  포트번호가 없음
 const router = require('./router/router.js');
+const DBrouter = require('./router/DBrouter.js')
+const EJSrouter = require('./router/EJSrouter.js');
+
 const bodyParser = require('body-parser');
 
+let ejs = require('ejs');
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended : false}));
 // post방식일 때 body영역을 분석해주는 미들웨어로 bodyparser 등록
 app.use(router);    // 미들웨어로 router를 등록
+app.use(DBrouter)
+app.use(EJSrouter);
 
 app.listen(3000);   // 현재 서버 파일의 port번호 등록
 
---
+++ mynodejs/router/EJSrouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const EJSrouter = express.Router(); 
+
+EJSrouter.get('/ejs01', (request, response) => {
+    console.log('/ejs01 라우터 실행')
+    response.render("ex01EJS", {
+        name1: 'value1',
+        name2: 'value2',
+    });
+});
+
+module.exports = EJSrouter;
